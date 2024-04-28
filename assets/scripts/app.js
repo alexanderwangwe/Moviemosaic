7b67fc6c3084c490b0a4d05433b99a68fc4041ce
Alexander Wangwe <alexanderwangwe@gmail.com>
2024-04-28 23:14:05 +0300

    Refactor renderNewMovieElement function to include movie ID in app.js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -21,7 +21,20 @@ const updateUI = () => {
   }
 };
 
-const renderNewMovieElement = (title, imageUrl, rating) => {
+const deleteMovieHandler = (movieId) => {
+  let movieIndex = 0;
+  for (const movie of movies) {
+    if (movie.id === movieId) {
+      break;
+    }
+    movieIndex++;
+  }
+  movies.splice(movieIndex, 1);
+  const listRoot = document.getElementById("movie-list");
+  listRoot.children[movieIndex].remove();
+};
+
+const renderNewMovieElement = (id, title, imageUrl, rating) => {
   const newMovieElement = document.createElement("li");
   newMovieElement.className = "movie-element";
   newMovieElement.innerHTML = `
@@ -33,6 +46,8 @@ const renderNewMovieElement = (title, imageUrl, rating) => {
             <p>${rating}/5 stars</p>
           </div>
         `;
+  newMovieElement.addEventListener("click", deleteMovieHandler.bind(null, Id));
+
   const listRoot = document.getElementById("movie-list");
   listRoot.append(newMovieElement);
 };
@@ -76,6 +91,7 @@ const addMovieHandler = () => {
   }
   // create a movie object
   const newMovie = {
+    id: Math.random().toString(),
     title: titleValue,
     image: imageUrlValue,
     rating: ratingValue,
@@ -86,7 +102,13 @@ const addMovieHandler = () => {
   toggleMovieModal();
   clearMovieInput();
   updateUI();
-  renderNewMovieElement(newMovie.title, newMovie.image, newMovie.rating);
+  renderNewMovieElement(
+    newMovie.id,
+    newMovie.title,
+    newMovie.image,
+    newMovie.rating
+  );
+  
 };
 
 // backdrop click handler
